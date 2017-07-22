1c805af63a50b08a8dbfc2434a648971f7dfcdd1
Philipp Frei <philipp.frei@freicomp.ch>
2017-07-22 16:09:25 +0200

    added simple webinterface

--- CasaSharp/CasaSharp/Program.cs
+++ CasaSharp/CasaSharp/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography;
-using System.Text;
+using System.Text; 
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
